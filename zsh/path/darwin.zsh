2a524bbc18c0e5ad2e36f2c67b22079d5ef7062c
Denis Vazhenin <denis.vazhenin@me.com>
2021-12-04 19:04:05 +0900

    Enable completion for google cloud sdk on Darwin

--- zsh/path/darwin.zsh
+++ zsh/path/darwin.zsh
@@ -10,3 +10,12 @@ if which jenv > /dev/null; then eval "$(jenv init -)"; fi
 
 # nvm
 [[ -f ${BREW_PREFIX}/opt/nvm/nvm.sh ]] && source ${BREW_PREFIX}/opt/nvm/nvm.sh
+
+# gcloud
+export CLOUDSDK_PYTHON="${BREW_PREFIX}/opt/python@3.8/libexec/bin/python"
+if [[ -f "${BREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc" ]]; then
+  source "${BREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+fi
+if [[ -f "${BREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc" ]]; then
+  source "${BREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+fi
